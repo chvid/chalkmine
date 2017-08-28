cdc873cc2c8e83067baa9ccc3086a043f18bdcb4
Christian Hvid <christian.hvid@gmail.com>
2017-08-28 11:29:45 +0200

    Introduced mapper interface and moved constructor mapping into DefaultMapper from QueryManager.

--- src/main/java/com/apelab/chalkmine/QueryManager.java
+++ src/main/java/com/apelab/chalkmine/QueryManager.java
@@ -13,114 +13,13 @@ import java.util.List;
  */
 
 public class QueryManager {
-    private Constructor findMatchingConstructor(Class klass, ResultSetMetaData metadata) throws SQLException {
-        for (Constructor c : klass.getDeclaredConstructors()) {
-            Class[] types = c.getParameterTypes();
+    public <T> T queryScalar(Connection connection, Mapper<T> mapper, String query, Object... parameters) throws SQLException {
+        List<T> l = queryList(connection, mapper, query, parameters);
 
-            boolean matches = true;
+        if (l.size() != 1)
+            throw new NonScalarException("Query " + query + " returned " + l.size() + " rows. Expected exactly 1 row.");
 
-            if (types.length == metadata.getColumnCount()) {
-                for (int i = 0; i < metadata.getColumnCount(); i++) {
-                    switch (metadata.getColumnType(i + 1)) {
-                        case Types.BIGINT:
-                        case Types.INTEGER:
-                        case Types.TINYINT:
-                            matches &= types[i].equals(int.class) || types[i].equals(long.class) ||
-                                    types[i].equals(Integer.class) || types[i].equals(Long.class) ||
-                                    types[i].equals(float.class) || types[i].equals(double.class) ||
-                                    types[i].equals(Float.class) || types[i].equals(Double.class);
-
-                            break;
-
-                        case Types.VARCHAR:
-                        case Types.CHAR:
-                            matches &= types[i].equals(String.class) || types[i].isEnum() ||
-                                    types[i].equals(boolean.class) || types[i].equals(Boolean.class);
-
-                            break;
-
-                        case Types.DATE:
-                        case Types.TIMESTAMP:
-                            matches &= types[i].equals(java.util.Date.class) || types[i].equals(java.sql.Date.class) ||
-                                    types[i].equals(java.sql.Timestamp.class);
-
-                            break;
-
-                        case Types.DECIMAL:
-                        case Types.DOUBLE:
-                        case Types.FLOAT:
-                            matches &= types[i].equals(float.class) || types[i].equals(double.class) ||
-                                    types[i].equals(Float.class) || types[i].equals(Double.class);
-
-                            break;
-
-                        default:
-                            matches &= true;
-                            break;
-                    }
-
-                }
-            } else {
-                matches = false;
-            }
-
-            if (matches) return c;
-        }
-
-        String message = klass.getName() + "(";
-
-        for (int i = 1; i <= metadata.getColumnCount(); i++) {
-            message += metadata.getColumnTypeName(i);
-            if (i != metadata.getColumnCount()) message += ", ";
-        }
-
-        message += ")";
-
-        throw new RuntimeException("Cannot find constructor matching " + message);
-    }
-
-    private boolean isSimpleType(Class klass) {
-        return klass.equals(Long.class) || klass.equals(long.class) || klass.equals(Integer.class) ||
-                klass.equals(Double.class) || klass.equals(String.class) || klass.equals(int.class) ||
-                klass.equals(double.class) || klass.equals(Date.class) || klass.equals(java.util.Date.class) ||
-                klass.equals(Boolean.class) || klass.equals(boolean.class) || klass.isEnum();
-    }
-
-    private <T> T readSimpleType(ResultSet rs, Class<T> klass, int index) throws SQLException {
-        if (klass.equals(Long.class)) {
-            return ((T) new Long(rs.getLong(index)));
-        } else if (klass.equals(long.class)) {
-            return ((T) new Long(rs.getLong(index)));
-        } else if (klass.equals(Integer.class)) {
-            return ((T) new Integer(rs.getInt(index)));
-        } else if (klass.equals(Double.class)) {
-            return ((T) new Double(rs.getDouble(index)));
-        } else if (klass.equals(String.class)) {
-            return ((T) rs.getString(index));
-        } else if ((klass.equals(Boolean.class)) || (klass.equals(boolean.class))) {
-            return ((T) (new Boolean("t".equalsIgnoreCase(rs.getString(index)))));
-        } else if (klass.equals(int.class)) {
-            return ((T) new Integer(rs.getInt(index)));
-        } else if (klass.equals(double.class)) {
-            return ((T) new Double(rs.getDouble(index)));
-        } else if (klass.equals(java.util.Date.class)) {
-            Timestamp timestamp = rs.getTimestamp(index);
-            if (timestamp != null) return ((T) new java.util.Date(timestamp.getTime()));
-            // else return (T) rs.getDate(index);
-        } else if (klass.equals(java.sql.Date.class)) {
-            return (T) rs.getDate(index);
-        } else if (klass.isEnum()) {
-            try {
-                return (T) klass.getMethod("valueOf", String.class).invoke(null, rs.getString(index));
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            } catch (InvocationTargetException e) {
-                throw new RuntimeException(e);
-            } catch (NoSuchMethodException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return null;
+        return l.get(0);
     }
 
     public <T> T queryScalar(Connection connection, Class<T> klass, String query, Object... parameters) throws SQLException {
@@ -130,6 +29,26 @@ public class QueryManager {
             throw new NonScalarException("Query " + query + " returned " + l.size() + " rows. Expected exactly 1 row.");
 
         return l.get(0);
+    }
+
+    public <T> List<T> queryList(Connection connection, Mapper<T> mapper, String query, Object... parameters) throws SQLException {
+        List<T> result = new ArrayList<T>();
+
+        PreparedStatement ps = connection.prepareStatement(query);
+
+        try {
+            addParametersToStatement(ps, parameters);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                result.add(mapper.map(rs));
+            }
+
+            return result;
+        } finally {
+            ps.close();
+        }
     }
 
     public <T> List<T> queryList(Connection connection, Class<T> klass, String query, Object... parameters) throws SQLException {
@@ -142,39 +61,13 @@ public class QueryManager {
 
             ResultSet rs = ps.executeQuery();
 
-            if (isSimpleType(klass)) {
-                while (rs.next()) {
-                    result.add(readSimpleType(rs, klass, 1));
-                }
-            } else {
-                ResultSetMetaData rsmd = rs.getMetaData();
+            Mapper<T> mapper = new DefaultMapper<T>(klass, rs.getMetaData());
 
-                Constructor<T> c = findMatchingConstructor(klass, rsmd);
-
-                c.setAccessible(true);
-
-                Class<?>[] parameterTypes = c.getParameterTypes();
-                Object constructorParameters[] = new Object[parameterTypes.length];
-
-                try {
-                    while (rs.next()) {
-                        for (int i = 0; i < parameterTypes.length; i++)
-                            constructorParameters[i] = readSimpleType(rs, parameterTypes[i], i + 1);
-
-                        result.add(c.newInstance(constructorParameters));
-                    }
-                } finally {
-                    c.setAccessible(false);
-                }
+            while (rs.next()) {
+                result.add(mapper.map(rs));
             }
 
             return result;
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
         } finally {
             ps.close();
         }
