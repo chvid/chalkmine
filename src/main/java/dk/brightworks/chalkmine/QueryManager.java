a16f3910879409f6164f6106d7cd53074296044f
Christian Hvid <christian.hvid@gmail.com>
2020-06-05 15:21:54 +0200

    Time queries parameter added.

--- src/main/java/dk/brightworks/chalkmine/QueryManager.java
+++ src/main/java/dk/brightworks/chalkmine/QueryManager.java
@@ -5,8 +5,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class QueryManager {
+    private static boolean TIME_QUERIES = System.getProperty("chalkmine.timeQueries", "false").equals("true");
+
+    private static Logger logger = Logger.getLogger(QueryManager.class.getName());
+
     public <T> T queryScalar(Connection connection, Mapper<T> mapper, String query, Object... parameters) throws SQLException {
         List<T> l = queryList(connection, mapper, query, parameters);
 
@@ -26,6 +31,8 @@ public class QueryManager {
     }
 
     public <T> List<T> queryList(Connection connection, Mapper<T> mapper, String query, Object... parameters) throws SQLException {
+        long startTime = TIME_QUERIES ? System.currentTimeMillis() : 0;
+
         List<T> result = new ArrayList<T>();
 
         PreparedStatement ps = connection.prepareStatement(query);
@@ -41,11 +48,16 @@ public class QueryManager {
 
             return result;
         } finally {
+            if (TIME_QUERIES) {
+                logger.info("queryList(" + query + ") completed in " + (System.currentTimeMillis() - startTime) + " msec.");
+            }
             ps.close();
         }
     }
 
     public <T> List<T> queryList(Connection connection, Class<T> klass, String query, Object... parameters) throws SQLException {
+        long startTime = TIME_QUERIES ? System.currentTimeMillis() : 0;
+
         List<T> result = new ArrayList<T>();
 
         PreparedStatement ps = connection.prepareStatement(query);
@@ -63,11 +75,16 @@ public class QueryManager {
 
             return result;
         } finally {
+            if (TIME_QUERIES) {
+                logger.info("queryList(" + query + ") completed in " + (System.currentTimeMillis() - startTime) + " msec.");
+            }
             ps.close();
         }
     }
 
-    public <K, V> Map<K, V> queryMap(Connection connection, Class<K> keyKlass, Class<V> valueKlass, String query, Object ... parameters) throws SQLException {
+    public <K, V> Map<K, V> queryMap(Connection connection, Class<K> keyKlass, Class<V> valueKlass, String query, Object... parameters) throws SQLException {
+        long startTime = TIME_QUERIES ? System.currentTimeMillis() : 0;
+
         Map<K, V> result = new HashMap<>();
 
         PreparedStatement ps = connection.prepareStatement(query);
@@ -87,11 +104,16 @@ public class QueryManager {
 
             return result;
         } finally {
+            if (TIME_QUERIES) {
+                logger.info("queryMap(" + query + ") completed in " + (System.currentTimeMillis() - startTime) + " msec.");
+            }
             ps.close();
         }
     }
 
     public int update(Connection connection, String statement, Object... parameters) throws SQLException {
+        long startTime = TIME_QUERIES ? System.currentTimeMillis() : 0;
+
         PreparedStatement ps = connection.prepareStatement(statement);
 
         try {
@@ -99,21 +121,28 @@ public class QueryManager {
 
             return ps.executeUpdate();
         } finally {
+            if (TIME_QUERIES) {
+                logger.info("update(" + statement + ") completed in " + (System.currentTimeMillis() - startTime) + " msec.");
+            }
             ps.close();
         }
     }
 
     private void addParametersToStatement(PreparedStatement ps, Object... parameters) throws SQLException {
-        for (int i = 0; i < parameters.length; i++)
-            if ((parameters[i] != null) && (parameters[i].getClass().equals(java.util.Date.class)))
+        for (int i = 0; i < parameters.length; i++) {
+            if ((parameters[i] != null) && (parameters[i].getClass().equals(java.util.Date.class))) {
                 ps.setTimestamp(i + 1, new Timestamp(((java.util.Date) parameters[i]).getTime()));
-            else if ((parameters[i] != null) && (parameters[i].getClass().isEnum()))
+            } else if ((parameters[i] != null) && (parameters[i].getClass().isEnum())) {
                 ps.setString(i + 1, "" + parameters[i]);
-            else if ((parameters[i] != null) && (parameters[i].getClass().equals(Boolean.class) || parameters[i].getClass().equals(boolean.class))) {
-                if ((Boolean) parameters[i])
+            } else if ((parameters[i] != null) && (parameters[i].getClass().equals(Boolean.class) || parameters[i].getClass().equals(boolean.class))) {
+                if ((Boolean) parameters[i]) {
                     ps.setString(i + 1, "t");
-                else
+                } else {
                     ps.setString(i + 1, "f");
-            } else ps.setObject(i + 1, parameters[i]);
+                }
+            } else {
+                ps.setObject(i + 1, parameters[i]);
+            }
+        }
     }
 }
