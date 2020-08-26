01ae3dd9f83fc2b31e7ca5effd070dcc037bb260
Christian Hvid <christian.hvid@gmail.com>
2020-08-26 15:25:20 +0200

    Preventing NPE on concurrent access.

--- src/main/java/dk/brightworks/chalkmine/PreparedStatementCache.java
+++ src/main/java/dk/brightworks/chalkmine/PreparedStatementCache.java
@@ -8,23 +8,28 @@ import java.util.Map;
 
 public class PreparedStatementCache {
     private static Map<Connection, Map<String, PreparedStatement>> cache = new HashMap<>();
+    private static Object lock = new Object();
 
     public static PreparedStatement createPreparedStatement(Connection connection, String query) throws SQLException {
-        if (!cache.containsKey(connection)) {
-            cache.put(connection, new HashMap<>());
+        synchronized (lock) {
+            if (!cache.containsKey(connection)) {
+                cache.put(connection, new HashMap<>());
+            }
+            if (!cache.get(connection).containsKey(query)) {
+                cache.get(connection).put(query, connection.prepareStatement(query));
+            }
+            return cache.get(connection).get(query);
         }
-        if (!cache.get(connection).containsKey(query)) {
-            cache.get(connection).put(query, connection.prepareStatement(query));
-        }
-        return cache.get(connection).get(query);
     }
 
     public static void closePreparedStatements(Connection connection) throws SQLException {
-        if (cache.containsKey(connection)) {
-            for (PreparedStatement ps : cache.get(connection).values()) {
-                ps.close();
+        synchronized (lock) {
+            if (cache.containsKey(connection)) {
+                for (PreparedStatement ps : cache.get(connection).values()) {
+                    ps.close();
+                }
+                cache.remove(connection);
             }
-            cache.remove(connection);
         }
     }
 
